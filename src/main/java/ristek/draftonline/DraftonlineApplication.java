b7ac2983194e93c9b8a167e5096fd0f87b03baba
indravb6 <mindrar96@gmail.com>
2018-02-17 19:45:50 +0700

    change to gradle

--- src/main/java/ristek/draftonline/DraftonlineApplication.java
+++ src/main/java/ristek/draftonline/DraftonlineApplication.java
@@ -2,10 +2,6 @@ package ristek.draftonline;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.repository.CrudRepository;
-import ristek.draftonline.model.Draft;
-
-import java.util.List;
 
 @SpringBootApplication
 public class DraftonlineApplication {
